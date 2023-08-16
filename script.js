0a5263b1c4950f9086a8d22ac847a62324f2fa68
GyHuba <gy.huba@gmail.com>
2023-08-16 19:18:16 +0200

    rock-paper-scissors ui

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let buttonsDOM = document.querySelector('.buttons');
+let rockBtn = document.querySelector(".rock");
+let paperBtn = document.querySelector(".paper");
+let scissorsBtn = document.querySelector(".scissors");
+let computerChoiceDOM = document.querySelector(".computer-choice");
+let resultDivDOM = document.querySelector(".result");
+let playerScoreDOM = document.querySelector(".player-score");
+let computerScoreDOM = document.querySelector(".computer-score");
+let gameOverDOM = document.querySelector(".game-over");
+let restartBtnDOM = document.querySelector(".restart-btn");
+
 let computerScore = 0;
 let playerScore = 0;
 
@@ -6,21 +17,11 @@ function getComputerChoice() {
     let tools = ["Rock", "Paper", "Scissors"];
     let randomNumber = Math.floor(Math.random() * 3)
     return tools[randomNumber]
-}
+};
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection !== "rock") {
-        if (playerSelection !== "paper") {
-            if (playerSelection !== "scissors") {
-                return "You can't choose this one!"
-            }
-        }
-    }
-    else if (playerSelection === undefined && playerSelection === null) {
-        return "You should pick something!"
-    }
-    console.log("The computer choose: " + computerSelection)
-    playerSelection = playerSelection.toLowerCase();
+
+    computerChoiceDOM.innerHTML = ("The computer choose: " + computerSelection)
 
     if (playerSelection === "rock") {
         if (computerSelection === "Rock") {
@@ -61,21 +62,68 @@ function playRound(playerSelection, computerSelection) {
             return "You win!"
         }
     }
-}
+};
 
-function game() {
-    for (let i = 5; i > 0; i--) {
-        console.log(playRound(prompt("Choose something!"), getComputerChoice()))
+function checkTheWinner() {
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore >= 5) {
+            gameOverDOM.innerHTML = "You win the game!"
+
+        }
+        else if (computerScore >= 5) {
+            gameOverDOM.innerHTML = "The computer win the game!"
+        }
+        gameOverDOM.classList.add("active");
+        restartBtnDOM.classList.add("active");
+        buttonsDOM.classList.add("none");
+    };
+};
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    showPoints();
+    gameOverDOM.classList.remove("active")
+    restartBtnDOM.classList.remove("active")
+    buttonsDOM.classList.remove("none")
+    resultDivDOM.innerHTML = "";
+    computerChoiceDOM.innerHTML = "";
+};
+
+function showPoints() {
+    playerScoreDOM.innerHTML = playerScore;
+    computerScoreDOM.innerHTML = computerScore;
+};
+
+function game(playerSelection) {
+    let result = playRound(playerSelection, getComputerChoice())
+    if (result === "You win!") {
+        resultDivDOM.style.color = "green"
     }
-    if (playerScore > computerScore) {
-        console.log("You win the game!")
-    }
-    else if (playerScore === computerScore) {
-        console.log("Draw")
+    else if (result === "The computer win!") {
+        resultDivDOM.style.color = "red"
     }
     else {
-        console.log("The computer win the game")
+        resultDivDOM.style.color = "gray"
     }
-}
+    resultDivDOM.innerHTML = result;
+    showPoints();
+    checkTheWinner();
+};
 
-game();
+rockBtn.addEventListener("click", (e) => {
+    playerSelection = (e.target.className);
+    game(playerSelection);
+});
+paperBtn.addEventListener("click", (e) => {
+    playerSelection = (e.target.className);
+    game(playerSelection);
+});
+scissorsBtn.addEventListener("click", (e) => {
+    playerSelection = (e.target.className);
+    game(playerSelection);
+});
+
+restartBtnDOM.addEventListener("click", () => {
+    restartGame();
+});
